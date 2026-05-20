f936791f1c186b38dd112c607553e99e0a519840
Uzoma Jeremiah Chike <mcjerry73@gmail.com>
2026-05-20 17:30:47 +0100

    fix types issue

--- src/mood-log/dto/update-mood-log.dto.ts
+++ src/mood-log/dto/update-mood-log.dto.ts
@@ -1,4 +1,18 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateMoodLogDto } from "./create-mood-log.dto";
+import { IsInt, IsNotEmpty, IsOptional, IsString, Max, Min } from "class-validator";
 
-export class UpdateMoodLogDto extends PartialType(CreateMoodLogDto) {}
+export class UpdateMoodLogDto {
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  mood?: string;
+
+  @IsOptional()
+  @IsInt()
+  @Min(1)
+  @Max(10)
+  score?: number;
+
+  @IsOptional()
+  @IsString()
+  note?: string;
+}
